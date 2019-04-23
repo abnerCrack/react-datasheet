6ce301c695dc3ff1ee719b88a2ca186c10139008
C <1129187719@qq.com>
2019-04-23 16:59:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import DataEditor from './DataEditor'
 import ValueViewer from './ValueViewer'
 import { renderValue, renderData } from './renderHelpers'
 
+// 默认导出整个表格
 export default DataSheet
 
+// 非默认导出(用于组合)
 export {
   Sheet,
   Row,
